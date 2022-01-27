907b5bfc6c60743c54c02137d5caf5a296a9e486
Luca Urbanowicz <human.exp11@gmail.com>
2022-01-26 17:50:23 -0700

    added current date

--- script.js
+++ script.js
@@ -1,77 +1,48 @@
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-// WHEN I scroll down
-// THEN I am presented with timeblocks for standard business hours
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-// WHEN I refresh the page
-// THEN the saved events persist
+var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+var currentHour = moment().format('h:mm:ss a');
 
-var textAreaList = ["sched9", "sched10", "sched11", "sched12", "sched13", 'sched14', "sched15", "sched16", "sched17", "sched18"];
-var textArealength = textAreaList.length;
-
-var saveBtnList = ["save9", "save10", "save11", "save12", "save13", "save14", "save15", "save16", "save17", "save18"]
-var saveBtnlength = saveBtnList.length;
-backgroundColor();
+var hour = moment().hours();
+var userInput;
+var hourSpan;
 
 
+var interval = setInterval(function() {
+    var momentNow = moment();
+    $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
+                        + momentNow.format('dddd')
+                         .substring(0,3).toUpperCase());
+    $('#currentDay').html(currentDate + " " + momentNow.format('hh:mm:ss A'));
+}, 100);
+  
 
-function set() {
-    for (var i = 0; i < textArealength; i++) {
-        localStorage.setItem(textAreaList[i], JSON.stringify(window[textAreaList[i]]));
-    }
-}
+// var textAreaList = ["sched9", "sched10", "sched11", "sched12", "sched13", 'sched14', "sched15", "sched16", "sched17", "sched18"];
+// var textArealength = textAreaList.length;
 
-function get() {
-    for (var i = 0; i < textArealength; i++) {
-        window[textAreaList[i]] = JSON.parse(localStorage.getItem(textAreaList[i]));
-    }
-}
-
-// Loads current date
-$(document).ready(function () {
-    function setPlanner() {
-        $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
-    }
-    //To clear text inputs and local storage
-    setPlanner();
-    $("#clearData").click(function () {
-        localStorage.clear();
-        location.reload();
-    });
-
-    //Change background based off past/present/future
-    function backgroundColor() {
-        $(".time-blocks").each(function () {
-            var specHour = parseInt($(this).attr("id"));
-            hour = parseInt(hour);
-            console.log(timeTest);
-            console.log(hour);
-            if (hour > specHour) {
-                $(this).addClass("past");
-            } else if (hour < specHour) {
-                $(this).addClass("future");
-            } else {
-                $(this).addClass("present");
-            }
-        });
-    }
-    $(document).ready(function () {
-        scheduleEvents()
-        backgroundColor()
-        // Save button
-        $(".saveBtn").on("click", function () {
-            userInput = $(this).siblings(".form-control").val().trim();
-            console.log(userInput);
-            hourSpan = $(this).siblings(".input-group-prepend").text().trim();
-            console.log(hourSpan);
-            localStorage.setItem(hourSpan, userInput);
-        })
-    });
+// var saveBtnList = ["save9", "save10", "save11", "save12", "save13", "save14", "save15", "save16", "save17", "save18"]
+// var saveBtnlength = saveBtnList.length;
+// backgroundColor();
+// set();
+// get();
 
 
+// function set() {
+//     for (var i = 0; i < textArealength; i++) {
+//         localStorage.setItem(textAreaList[i], JSON.stringify(window[textAreaList[i]]));
+//     }
+// };
+
+// function get() {
+//     for (var i = 0; i < textArealength; i++) {
+//         window[textAreaList[i]] = JSON.parse(localStorage.getItem(textAreaList[i]));
+//     }
+// };
+
+// displayColor();
+// }
+
+
+// Button clear schedule
+$("#clearData").on("click", function(){
+    localStorage.clear();
+    initPage()
+  }) 
